21f03785d98b870920826348b5f5fc94a7ec4d35
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2025-01-07 13:45:06 -0300

    Adicionando a funcionalidade de quando selecionar outra data os horários fiquem todos disponíveis

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,7 @@ import dayjs from "dayjs"
 
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 // Date atual para formatar o input
@@ -17,5 +18,31 @@ form.onsubmit = (event) => {
   // Previne o comportamento padrão de recarregar a página.
   event.preventDefault()
 
-  console.log("Enviando!")
+  try {
+    // Recuperando o nome do cliente.
+    const name = clientName.value.trim()
+    // verifica se o nome do cliente foi colocado.
+    if(!name){
+      return alert("Informe o nome do cliente!")
+    }
+    // Recupera o horário selecionado.
+    const hourSelected = document.querySelector(".hour-selected")
+    // Verifica se o horário foi selecionado ou não, caso não seja retorna um mensagem de alerta.
+    if(!hourSelected){
+      return alert("Selecione um horário.")
+    }
+
+    //Recupera apenas a hora.
+    const [hour] = hourSelected.innerText.split(":")
+    
+    // Insere a hora na data do agendamento
+    const when = dayjs(selectedDate.value).add(hour, "hour")
+
+    // Gera um ID
+    const id = new Date().getTime()
+    
+  } catch (error) {
+    alert("Não foi possível realizar o agendamento.")
+    console.log(error)
+  }
 }
