c112a26f69dcf33d9acaa47f26c4b198e04803f7
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2025-01-07 16:37:43 -0300

    Fazendo uma verificação na hora e na data do agendamento

--- src/modules/form/hours-load.js
+++ src/modules/form/hours-load.js
@@ -4,9 +4,14 @@ import { hoursClick } from "./hours-click.js"
 
 const hours = document.getElementById("hours")
 
-export function hoursLoad({date}){
+export function hoursLoad({date, dailySchedules}){
   // Limpa a lista de horários.
   hours.innerHTML = ""
+
+  // Obtém a lista de todos os horários ocupados
+  const unavailableHours = dailySchedules.map((schedule)=>{
+    dayjs(schedule.when).format("HH:mm")
+  })
 
   const opening = openingHours.map((hour)=>{
 
@@ -15,12 +20,13 @@ export function hoursLoad({date}){
     
 
     // Adiciona a hora  na data e verifica se está no passado.
-    const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs())
-    console.log(scheduleHour, isHourPast)
+    const isHourPast = dayjs(date).add(scheduleHour, "hour").isBefore(dayjs())
+    
+    const available = !unavailableHours.includes(hour) && !isHourPast
     
     return {
       hour,
-      available: isHourPast,
+      available
     }
   })
 
