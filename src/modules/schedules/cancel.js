ea186ebfb7ea25b86df14ac15373146691518af6
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2025-01-08 13:10:59 -0300

    adicionando mais configurações a funcionalidade de cancelamento

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,2 +1,19 @@
 const periods = document.querySelectorAll(".period")
-console.log(periods)
+
+// Gerar evento de click para cada lista (manhã, tarde e noite)
+periods.forEach((period)=>{
+  // Captura o evento de clique na lista.
+  period.addEventListener("click", (event)=>{
+   if(event.target.classList.contains("cancel-icon")){
+    // Obtém a li pai do elemento clicado.
+    const item = event.target.closest("li")
+    // Desestrutura e pega apenas o id do item
+    const {id} = item.dataset
+
+    if(id){
+      const isConfirm = confirm("Tem certeza que deseja cancelar?")
+    }
+    
+   }
+  })
+})
