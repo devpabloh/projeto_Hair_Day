f94bca81dd813042b96bf2807e1d549742fb64ff
Dev. Pablo H. <146978584+devpabloh@users.noreply.github.com>
2025-01-08 13:32:12 -0300

    Finalizando a funcionalidade de cancelamento de agendamento.

--- src/modules/schedules/cancel.js
+++ src/modules/schedules/cancel.js
@@ -1,18 +1,33 @@
+import {schedulesDay} from "./load.js"
+import { scheduleCancel } from "../../services/schedule-cancel.js"
+
 const periods = document.querySelectorAll(".period")
 
 // Gerar evento de click para cada lista (manhã, tarde e noite)
 periods.forEach((period)=>{
   // Captura o evento de clique na lista.
-  period.addEventListener("click", (event)=>{
+  period.addEventListener("click", async (event)=>{
    if(event.target.classList.contains("cancel-icon")){
     // Obtém a li pai do elemento clicado.
     const item = event.target.closest("li")
-    // Desestrutura e pega apenas o id do item
+    // Desestrutura e pega apenas o id do item que queremos remover.
     const {id} = item.dataset
 
+    // Confirma que o ID foi selecionado.
     if(id){
-      const isConfirm = confirm("Tem certeza que deseja cancelar?")
+      // Confirma e o usuário realmente quer remover aquele agendamento.
+      const isConfirm = confirm("Tem certeza que deseja cancelar o agendamento?")
+
+      if(isConfirm){
+        // Faz a requisição na API para cancelar.
+        await scheduleCancel({id})
+
+        // Recarrega os agendamentos.
+        schedulesDay()
+      }
     }
+
+  
     
    }
   })
